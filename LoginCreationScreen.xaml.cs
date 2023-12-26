356d3c76436060d9ab71f4325d6eb372bfcc2561
T-Howle06 <T.Howle06@outlook.com>
2023-12-26 17:28:30 +0000

    login creation screen created

--- LoginCreationScreen.xaml.cs
+++ LoginCreationScreen.xaml.cs
@@ -1,31 +1,33 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Runtime.CompilerServices;
 
 namespace DoncasterUTCStudents
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginCreationScreen.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginCreationScreen : Window
     {
-        public MainWindow()
+        public LoginCreationScreen()
         {
             InitializeComponent();
         }
 
-        private bool validateLogin(string username, string password)
+        private bool addToDatabase(string username, string password)
         {
-            string query = "SELECT COUNT(*) FROM logins WHERE username = @username AND password = @password";
+            string query = "INSERT INTO logins (username, password) VALUES (@username, @password)";
 
             try
             {
@@ -38,39 +40,33 @@ namespace DoncasterUTCStudents
                         command.Parameters.AddWithValue("@username", username);
                         command.Parameters.AddWithValue("@password", password);
 
-                        int count = (int)command.ExecuteScalar();
+                        int rowsAffected = command.ExecuteNonQuery();
 
-                        return count > 0;
+                        return rowsAffected > 0;
                     }
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error: {ex.Message}", "Login Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show($"Error: {ex.Message}", "Login Creation Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return false;
             }
-            
         }
 
-        private void createNewLoginLink_Click(object sender, EventArgs e)
-        {
-            LoginCreationScreen loginCreationScreen = new LoginCreationScreen();
-            loginCreationScreen.Show();
-            this.Hide();
-        }
-
-        private void loginButton_Click(object sender, RoutedEventArgs e)
+        private void createButton_Click(object sender, RoutedEventArgs e)
         {
             string username = usernameTextBox.Text;
             string password = SharedFunctions.hashPassword(passwordPasswordBox.Password);
 
-            if (SharedFunctions.validLogin(usernameTextBox, passwordPasswordBox, username, password) && validateLogin(username, password))
+            if (addToDatabase(username, password))
             {
-                MessageBox.Show("Valid login.", "Valid Login.", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            else if (!validateLogin(username, password))
-            {
-                MessageBox.Show("Please enter a valid login or create a new login.", "Invalid Login.", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBoxResult result = MessageBox.Show("Would you like to login in?", "Login Creation Successful", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                if (result == MessageBoxResult.Yes)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Hide();
+                }
             }
         }
 
@@ -83,7 +79,7 @@ namespace DoncasterUTCStudents
         private void exitButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Hand);
-        
+
             if (result == MessageBoxResult.Yes)
             {
                 Application.Current.Shutdown();
---
+++ SharedFunctions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DoncasterUTCStudents
+{
+    public static class SharedFunctions
+    {
+        public static string connectionString = "Data Source=DESKTOP-RVQ5B1D\\SQLEXPRESS;Initial Catalog=DoncasterUTC;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+
+        public static string hashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < hashedBytes.Length; i++)
+                {
+                    stringBuilder.Append(hashedBytes[i].ToString("x2"));
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+
+        public static bool validLogin(TextBox usernameTextBox, PasswordBox passwordPasswordBox, string username, string password)
+        {
+            if (username == string.Empty)
+            {
+                MessageBox.Show("Please enter a username.", "Invalid Login.", MessageBoxButton.OK, MessageBoxImage.Error);
+                usernameTextBox.Focus();
+                return false;
+            }
+            else if (password == string.Empty)
+            {
+                MessageBox.Show("Please enter a password.", "Invalid Login.", MessageBoxButton.OK, MessageBoxImage.Error);
+                passwordPasswordBox.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}

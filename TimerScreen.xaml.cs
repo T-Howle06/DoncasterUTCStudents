4ccedc4b66ea4bf0ee9d2dddbaa7e784644bb040
T-Howle06 <T.Howle06@outlook.com>
2024-01-01 20:30:12 +0000

    timer screen created

--- TimerScreen.xaml.cs
+++ TimerScreen.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DoncasterUTCStudents
 {
@@ -19,9 +20,84 @@ namespace DoncasterUTCStudents
     /// </summary>
     public partial class TimerScreen : Window
     {
+        private DispatcherTimer timer;
+        private int pomodoroDuration = 25;
+        private int breakDuration = 5;
+        private int secondsRemaining;
+
         public TimerScreen()
         {
             InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (secondsRemaining > 0)
+            {
+                secondsRemaining--;
+                updateTimerText();
+            }
+            else
+            {
+                timer.Stop();
+
+                if (pomodoroDuration > 0)
+                {
+                    MessageBox.Show("Pomodoro completed! Take a break.");
+                    secondsRemaining = breakDuration * 60;
+                    pomodoroDuration = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Break completed! Start Pomodoro.");
+                    secondsRemaining = pomodoroDuration * 60;
+                    pomodoroDuration = 25; // Reset to Pomodoro duration
+                }
+
+                updateTimerText();
+                timer.Start();
+            }
+        }
+
+        private void updateTimerText()
+        {
+            int minutes = secondsRemaining / 60;
+            int seconds = secondsRemaining % 60;
+            timerText.Text = $"{minutes:D2}:{seconds:D2}";
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            HomeScreen homeScreen = new HomeScreen();
+            homeScreen.Show();
+            this.Hide();
+        }
+
+        private void startTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            secondsRemaining = pomodoroDuration * 60;
+            updateTimerText();
+            timer.Start();
+        }
+
+        private void pauseTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void resetTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            secondsRemaining = 0;
+            updateTimerText();
         }
     }
 }
